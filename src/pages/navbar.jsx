5a9359bdbc3d4addf124f844a12acf9c20d8aeea
Joshua Waters <joshua.waters365@gmail.com>
2023-12-08 09:54:50 -0500

    Home and Navbar components

--- src/pages/navbar.jsx
+++ src/pages/navbar.jsx
@@ -1,16 +1,19 @@
 import { Link } from "react-router-dom";
+import React from "react";
 
-const Navbar = () => {
-  return (
-    <nav id="nb">
-      <ul id="nb-list">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/button">Button Project</Link></li>
-        <li><Link to="/tictactoe">Tic-Tac-Toe</Link></li>
-        <li><Link to="/forms">Forms Project</Link></li>
-      </ul>
-    </nav>
-  );
+class Navbar extends React.Component {
+    render(){
+      return (
+        <nav id="nb">
+          <ul id="nb-list">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/button">Button Project</Link></li>
+            <li><Link to="/tictactoe">Tic-Tac-Toe</Link></li>
+            <li><Link to="/forms">Forms Project</Link></li>
+          </ul>
+        </nav>
+      );
+  }
 }
 
 export default Navbar;
--- src/pages/start.js
+++ src/pages/start.js
@@ -1,14 +1,16 @@
 import React from "react";
 
-const Start = () => {
-  return (
-    <div>
-      <h1 id="home">Welcome to Joshua Waters' React Projects!</h1>
-      <p>
-        
-      </p>
-    </div>
-  );
+class Start extends React.Component{
+  state = {}
+  
+  render(){
+    return (
+      <div>
+        <h1 id="home">Welcome to Joshua Waters' React Projects!</h1>
+      </div>
+    );
+  }
 }
+
 
 export default Start;
